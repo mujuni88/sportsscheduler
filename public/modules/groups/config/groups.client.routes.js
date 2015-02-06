0e82d0d3d28b64e09668848d9d2befc8c6b1a16e
Joe Buza <joebm08@gmail.com>
2015-02-06 16:23:14 -0600

    Added members and events initial views

--- public/modules/groups/config/groups.client.routes.js
+++ public/modules/groups/config/groups.client.routes.js
@@ -2,24 +2,32 @@
 
 //Setting up route
 angular.module('groups').config(['$stateProvider',
-	function($stateProvider) {
-		// Groups state routing
-		$stateProvider.
-		state('listGroups', {
-			url: '/groups',
-			templateUrl: 'modules/groups/views/list-groups.client.view.html'
-		}).
-		state('createGroup', {
-			url: '/groups/create',
-			templateUrl: 'modules/groups/views/create-group.client.view.html'
-		}).
-		state('viewGroup', {
-			url: '/groups/:groupId',
-			templateUrl: 'modules/groups/views/view-group.client.view.html'
-		}).
-		state('editGroup', {
-			url: '/groups/:groupId/edit',
-			templateUrl: 'modules/groups/views/edit-group.client.view.html'
-		});
-	}
+    function ($stateProvider) {
+        // Groups state routing
+        $stateProvider.
+            state('listGroups', {
+                url: '/groups',
+                templateUrl: 'modules/groups/views/list-groups.client.view.html'
+            }).
+            state('createGroup', {
+                url: '/groups/create',
+                templateUrl: 'modules/groups/views/create-group.client.view.html'
+            }).
+            state('viewGroup', {
+                url: '/groups/:groupId',
+                templateUrl: 'modules/groups/views/view-group.client.view.html'
+            }).
+            state('viewGroup.members', {
+                url: '/members',
+                templateUrl: 'modules/groups/views/view-members-group.client.view.html'
+            }).
+            state('viewGroup.events', {
+                url: '/events',
+                templateUrl: 'modules/groups/views/view-events-group.client.view.html'
+            }).
+            state('editGroup', {
+                url: '/groups/:groupId/edit',
+                templateUrl: 'modules/groups/views/edit-group.client.view.html'
+            });
+    }
 ]);
