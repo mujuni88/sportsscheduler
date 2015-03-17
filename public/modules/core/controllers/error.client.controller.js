54adb0d19e741b6e660a0e084dcae08d6c0e73d5
Joe Buza <joebm08@gmail.com>
2015-03-17 17:18:32 -0500

    Clear errors on route change

--- public/modules/core/controllers/error.client.controller.js
+++ public/modules/core/controllers/error.client.controller.js
@@ -4,12 +4,24 @@
 angular.module('core').controller('ErrorController', ['$rootScope','lodash', ErrorController]);
 
 function ErrorController($rootScope, _) {
-    $rootScope.$watch('error', function(){
-        if(_.isUndefined($rootScope.error)){return;}
-        $rootScope.errors = $rootScope.error.clientMessage.concat($rootScope.error.devMessage);
-    });
-    
+
+    $rootScope.$on('$stateChangeStart', onRouteChange);
+    $rootScope.$watch('error', watchError);
     $rootScope.closeAlert = closeAlert;
+
+    function onRouteChange(){
+        if(isErrorUnDef()){return;}
+        $rootScope.errors = [];
+    }
+    
+    function watchError(){
+        if(isErrorUnDef()){return;}
+        $rootScope.errors = $rootScope.error.clientMessage.concat($rootScope.error.devMessage);
+    }
+    
+    function isErrorUnDef(){
+       return _.isUndefined($rootScope.error); 
+    }
     
     function closeAlert(index){
         $rootScope.errors.splice(index, 1);
