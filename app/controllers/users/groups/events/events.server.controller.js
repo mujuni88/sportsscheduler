ea1ee4641949591063b04ab6c175f886d2b41fde
Zidane <trey.gaines@navagis.com>
2015-04-20 14:34:31 -0500

    commit for ticket #33

--- app/controllers/users/groups/events/events.server.controller.js
+++ app/controllers/users/groups/events/events.server.controller.js
@@ -83,6 +83,37 @@ exports.create = function(req, res) {
 exports.read = function(req, res) {
 	//console.log(req.event);
 	//res.jsonp(req.event);
+
+	var myResponse = new MyResponse();
+	var eventID = req.params.eventId;
+
+	if(!Helper.isValidObjectID(eventID))
+	{
+		myResponse.addMessages(serverJSON.api.events._id.invalid);
+		Helper.output(myResponse,res);
+		
+		return;
+	}
+
+	var query = {
+		_id: eventID
+	};
+
+	Helper.find(EventModel,query,function(err,mod) {
+
+		if(err || mod.length === 0)
+		{
+			myResponse.addMessages(serverJSON.api.events._id.exist);
+			Helper.output(myResponse,res);
+
+			return;
+		}
+
+		Helper.populateModel(EventModel,mod,EventModel.errPath,function(mod) {
+			myResponse.setData(mod);
+			Helper.output(myResponse,res);
+		});
+	});
 };
 
 /**
