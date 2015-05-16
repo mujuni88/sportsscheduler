033fd4ae12b7ef44089c32c58abdcc7f60e64ab5
Zidane <trey.gaines@navagis.com>
2015-05-16 09:21:57 -0500

    commit for #61

--- app/controllers/users/groups/events/events.server.controller.js
+++ app/controllers/users/groups/events/events.server.controller.js
@@ -360,10 +360,6 @@ exports.listEventsForGroup = function(req, res) {
 					myResponse.transformMongooseError(EventModel.errPath,String(err));
 					Helper.output(EventModel,null,myResponse,res);
 				}
-				else if(mod.length === 0 || !mod) {
-					myResponse.addMessages(serverJSON.api.events._id.exist);
-					Helper.output(EventModel,null,myResponse,res);
-				}
 				else
 					Helper.output(EventModel,mod,myResponse,res);				
 			});
