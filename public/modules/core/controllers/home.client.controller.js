3541ba8e96c33ed1a40b5fb4ae078284ecdcb44f
Joe Buza <joebm08@gmail.com>
2015-06-23 17:00:33 -0500

    Changed homepage

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,19 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		var slides = $scope.slides = [];
+		
+		for (var i=0; i<4; i++) {
+			addSlides();
+		}
+
+		function addSlides() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				image: 'http://placekitten.com/' + newWidth + '/300',
+				text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+				['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+			});
+		}
 	}
 ]);
