514876ce9837ef34e8f80bee140b121140ddb1a6
Joe Buza <joebm08@gmail.com>
2015-06-24 12:19:57 -0500

    Fixed slide show

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','$animate',
+	function($scope, Authentication, $animate) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$animate.enabled(false);
 		var slides = $scope.slides = [];
 		
 		for (var i=0; i<4; i++) {
