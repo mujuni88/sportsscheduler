dd91e3e3774e37c1409bdb2b2d7ccd8f834f0669
Joe Buza <joebm08@gmail.com>
2015-08-20 17:05:16 -0500

    Removed env production on default task

--- gruntfile.js
+++ gruntfile.js
@@ -244,7 +244,7 @@ module.exports = function (grunt) {
     });
 
     // Default task(s).
-    grunt.registerTask('default', ['env:prod','lint', 'concurrent:default']);
+    grunt.registerTask('default', ['lint', 'concurrent:default']);
 
     // Documentation task
     grunt.loadNpmTasks('grunt-jsdoc');
